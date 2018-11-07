a65749fb7243272736784b10bf1e5fba56d6d8c6
Giedrius Jocius <giedrius.jocius@devbridge.com>
2018-11-07 16:06:06 +0200

    Fixed apk path for android

--- detox/src/devices/android/APKPath.js
+++ detox/src/devices/android/APKPath.js
@@ -6,21 +6,10 @@ class APKPath {
 
   static getTestApkPath(originalApkPath) {
     const originalApkPathObj = path.parse(originalApkPath);
-    let tempPath = originalApkPathObj.dir.split(path.sep);
     const splitFileName = originalApkPathObj.name.split('-');
-
     const buildType = _.last(splitFileName);
-    const flavorDimensions = _.slice(splitFileName, 1, splitFileName.length - 1);
-
-    tempPath = _.dropRight(tempPath, 1); //buildType
-
-    let flavorDimensionsPath = '';
-    if (flavorDimensions.length > 0) {
-      flavorDimensionsPath = string.lowerCamelCaseJoin(flavorDimensions);
-      tempPath = _.dropRight(tempPath, 1); //flavorDimensions
-    }
-
-    const testApkPath = path.join(tempPath.join(path.sep), 'androidTest', flavorDimensionsPath, buildType, `${originalApkPathObj.name}-androidTest${originalApkPathObj.ext}`);
+    const testApkPath = path.join(originalApkPathObj.dir, 'androidTest', buildType, `app-${buildType}-androidTest${originalApkPathObj.ext}`);
+    
     return testApkPath;
   }
 }
